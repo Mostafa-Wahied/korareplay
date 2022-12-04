b9063f10de9fbabb14578e762dc558d2b3c5f0a0
Mostafa Wahied <97263286+Mostafa-Wahied@users.noreply.github.com>
2022-12-04 10:07:07 -0800

    dec4th part 1, search included

--- landing/script.js
+++ landing/script.js
@@ -2,20 +2,21 @@ function searchFunction() {
     // declare variables
     var input, filter, card, headers, headerText, txtValue;
     input = document.getElementById('search-input');
-    filter = input.value.toUpperCase();
     card = document.getElementById("card-row");
     headers = card.getElementsByClassName('card-header');
-    console.log(headers)
+
+    // console.log(headers);
+    // console.log(input.value);
 
     // loop through all card headers, and hide those who don't match the search query
     for (i = 0; i < headers.length; i++) {
         headerText = headers[i].innerText;
-        console.log(headerText)
+        // console.log(headerText)
         txtValue = headerText.innerText || headerText.textContent;
-        if (input.value.length === false) {
-            headers[i].style.display = "";
+        if (!headerText.includes(input.value)) {
+            headers[i].parentElement.style.display = "none";
         } else {
-            headers[i].style.display = "none";
-        }
+            headers[i].parentElement.style.display = "";
+                }
     }
 }
