0f0a38144e0199bd228d4d4ac3e222ef7209050f
Mostafa Wahied <97263286+Mostafa-Wahied@users.noreply.github.com>
2022-12-23 00:45:31 -0800

    add pagination

--- landing/script.js
+++ landing/script.js
@@ -31,6 +31,77 @@ function loadGoogleTranslate() {
 }
 // end of translation function
 
+// pagination function
+function getPageList(totalPages, page, maxLength) {
+    function range(start, end) {
+        return Array.from(Array(end - start + 1), (_, i) => i + start);
+    }
+
+    var sideWidth = maxLength < 9 ? 1 : 2;
+    var leftWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+    var rightWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+
+    if (totalPages <= maxLength) {
+        return range(1, totalPages);
+    }
+
+    if (page <= maxLength - sideWidth - 1 - rightWidth) {
+        return range(1, maxLength - sideWidth - 1).concat(0, range(totalPages - sideWidth + 1, totalPages));
+    }
+
+    if (page >= totalPages - sideWidth - 1 - rightWidth) {
+        return range(1, sideWidth).concat(0, range(totalPages - sideWidth - 1 - rightWidth - leftWidth, totalPages));
+    }
+
+    return range(1, sideWidth).concat(0, range(page - leftWidth, page + rightWidth), 0, range(totalPages - sideWidth + 1, totalPages));
+}
+
+$(function () {
+    var numberOfItems = $(".card-row .col").length;
+    var limitPerPage = 16; //how many card items visible per page
+    var totalPages = Math.ceil(numberOfItems / limitPerPage);
+    var paginationSize = 5; //how many page visible in pagination
+    var currentPage;
+
+    function showPage(whichPage) {
+        if (whichPage < 1 || whichPage > totalPages) return false;
+
+        currentPage = whichPage;
+
+        $(".card-row .col").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+
+        $(".paginations li").slice(1, -1).remove();
+
+        getPageList(totalPages, currentPage, paginationSize).forEach(item => {
+            $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots").toggleClass("active", item === currentPage).append($("<a>").addClass("page-link").attr({ href: "javascript:void(0)" }).text(item || "...")).insertBefore(".next-page");
+        });
+
+        $(".previous-page").toggleClass("disable", currentPage === 1);
+        $(".next-page").toggleClass("disable", currentPage === totalPages);
+        return true;
+    }
+
+    $(".paginations").append(
+        $("<li>").addClass("page-item").addClass("previous-page").append($("<a>").addClass("page-link").attr({ href: "javascript:void(0)" }).text("Prev")),
+        $("<li>").addClass("page-item").addClass("next-page").append($("<a>").addClass("page-link").attr({ href: "javascript:void(0)" }).text("Next"))
+    );
+
+    $(".card-row").show();
+    showPage(1);
+
+    $(document).on("click", ".paginations li.current-page:not(.active)", function () {
+        return showPage(+$(this).text());
+    });
+
+    $(".next-page").on("click", function() {
+        return showPage(currentPage + 1);
+    })
+    $(".previous-page").on("click", function() {
+        return showPage(currentPage - 1);
+    })
+});
+// end of pagination function
+
 
 // tabs function
 function openCity(evt, cityName) {
@@ -49,3 +120,4 @@ function openCity(evt, cityName) {
 }
 document.getElementById("defaultOpen").click();
 // end of tabs function
+
